120c54887ecdaa6b1cfaef41089fc05e003a3165
Douglas237 <lyonneldouglas@gmail.com>
2023-03-30 06:36:47 +0200

    modiffication du dashboard

--- public/js/finance.js
+++ public/js/finance.js
@@ -1,21 +1,37 @@
-$('.sidebar .grandul .client-btn').click(function () {
-    $('.sidebar .grandul .souscli').toggleClass("show");
-    $('.sidebar .grandul .toggle1').toggleClass("rotate1");
+$(".sidebar .grandul .client-btn").click(function () {
+    $(".sidebar .grandul .souscli").toggleClass("show");
+    $(".sidebar .grandul .toggle1").toggleClass("rotate1");
 });
-$('.sidebar .grandul .compte-btn').click(function () {
-    $('.sidebar .grandul .souscpt').toggleClass("show2");
-    $('.sidebar .grandul .toggle2').toggleClass("rotate2");
+$(".sidebar .grandul .compte-btn").click(function () {
+    $(".sidebar .grandul .souscpt").toggleClass("show2");
+    $(".sidebar .grandul .toggle2").toggleClass("rotate2");
 });
-$('.navbare .navbar .content .dropdown').click(function () {
-    $('.navbare .navbar .content .elmts').toggleClass("show3");
-    $('.navbare .navbar .content .toggle').toggleClass("rotate3");
+$(".navbare .navbar .content .dropdown").click(function () {
+    $(".navbare .navbar .content .elmts").toggleClass("show3");
+    $(".navbare .navbar .content .toggle").toggleClass("rotate3");
     // alert('bomjoure');
 });
 
 // gestion du menu
-$('#menu_icon').click(function () {
-    $('#sidebar').toggleClass('hide');
-    $('#navbare').toggleClass('ajout');
-    $('#centre').toggleClass('ajout');
-    console.log('oui');
+// $('#menu_icon').click(function () {
+//     $('#sidebar').toggleClass('hide');
+//     $('#navbare').toggleClass('ajout');
+//     $('#centre').toggleClass('ajout1');
+//     console.log('oui');
+// });
+$("#sidebar").hover(function () {
+    $("#sidebar").addClass("augmenter");
+    $("#navbare").addClass("ajout");
+    $("#centre").addClass("ajout1");
+    console.log("oui");
+});
+
+$("#sidebar").mouseleave(function () {
+    $("#sidebar").removeClass("augmenter");
+    $("#navbare").removeClass("ajout");
+    $("#centre").removeClass("ajout1");
+    $(".sidebar .grandul .souscli").removeClass("show");
+    $(".sidebar .grandul .souscpt").removeClass("show2");
+    $(".navbare .navbar .content .elmts").removeClass("show3");
+    console.log("oui");
 });
